3509beb04ad5c8419fe7a3952399483dcddb5c0a
kapishash <kapishashtankar10@gmail.com>
2025-01-15 20:53:53 +0530

    experimants

--- network_security/components/model_trainer.py
+++ network_security/components/model_trainer.py
@@ -63,7 +63,7 @@ class ModelTrainer:
                 # There are other ways to use the Model Registry, which depends on the use case,
                 # please refer to the doc for more information:
                 # https://mlflow.org/docs/latest/model-registry.html#api-workflow
-                mlflow.sklearn.log_model(best_model, "model", registered_model_name=best_model)
+                mlflow.sklearn.log_model(best_model, "model")  # , registered_model_name=best_model
             else:
                 mlflow.sklearn.log_model(best_model, "model")
         
@@ -80,26 +80,26 @@ class ModelTrainer:
             "Decision Tree": {
                 # 'criterion':['gini', 'entropy', 'log_loss'],
                 # 'splitter':['best','random'],
-                # 'max_features':['sqrt','log2'],
+                'max_features':['sqrt','log2'],
             },
             "Random Forest":{
-                # 'criterion':['gini', 'entropy', 'log_loss'],
+                'criterion':['gini', 'entropy', 'log_loss'],
                 
                 # 'max_features':['sqrt','log2',None],
-                # 'n_estimators': [8,16,32,128,256]
+                'n_estimators': [8,16,32,128,256]
             },
             "Gradient Boosting":{
-                # 'loss':['log_loss', 'exponential'],
-                # 'learning_rate':[.1,.01,.05,.001],
+                'loss':['log_loss', 'exponential'],
+                'learning_rate':[.1,.01,.05,.001],
                 # 'subsample':[0.6,0.7,0.75,0.85,0.9],
-                # 'criterion':['squared_error', 'friedman_mse'],
+                'criterion':['squared_error', 'friedman_mse'],
                 # 'max_features':['auto','sqrt','log2'],
-                # 'n_estimators': [8,16,32,64,128,256]
+                'n_estimators': [8,16,32,64,128,256]
             },
             "Logistic Regression":{},
             "AdaBoost":{
-                # 'learning_rate':[.1,.01,.001],
-                # 'n_estimators': [8,16,32,64,128,256]
+                'learning_rate':[.1,.01,.001],
+                'n_estimators': [8,16,32,64,128,256]
             }
             
         }
